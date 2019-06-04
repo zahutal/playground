67440d8f55c5ddd70a1b75fd6b81d1a1c3f5aadb
Ulinator <zahuta@gmail.com>
2019-06-04 14:14:47 +0200

    added docs folder

---
+++ react-pommodoro/docs/precache-manifest.c09a97e4eefe8a2fcfcc8e30a042a6e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff20d4323d1c9a368b2a05ef3fceaecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63819da59748ea6820a2",
+    "url": "/static/css/main.0a720bfd.chunk.css"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "63819da59748ea6820a2",
+    "url": "/static/js/main.fea39114.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad8d347b1f1d42d5013c260c23653dba",
+    "url": "/static/media/pommodoro.ad8d347b.png"
+  }
+]);
